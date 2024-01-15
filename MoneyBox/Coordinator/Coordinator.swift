11f2ecfc193c6f29fa85fc785a8033f8ca39b3c4
Shafique Dassu <shafd1231@gmail.com>
2024-01-15 11:34:51 +0000

    Added documentation for coordinator file

--- MoneyBox/Coordinator/Coordinator.swift
+++ MoneyBox/Coordinator/Coordinator.swift
@@ -9,7 +9,8 @@ import Foundation
 import UIKit
 import Networking
 
-class AppCoordinator: NSObject {
+/// The AppCoordinator is responsible for navigation throughout the entire app
+class AppCoordinator {
 
     private var window: UIWindow?
     private var navigationController: UINavigationController?
@@ -18,52 +19,68 @@ class AppCoordinator: NSObject {
         self.window = window
     }
     
+    /**
+     Provides list of potential view controllers to navigate to
+     */
     enum Destination {
         case login, userAccounts, userAccountDetail
     }
     
+    /**
+     Determines the first view controller that will be shown after app launch
+     */
     func start() {
         let loginViewModel = LoginViewModel(appCoordinator: self)
         let loginViewController = LoginViewController(viewModel: loginViewModel)
-        //loginViewController.viewModel.appCoordinator = self
         window!.rootViewController = loginViewController
         window!.makeKeyAndVisible()
     }
     
+    /**
+     Method that takes us to UserAccounts view controller
+        - Parameters:
+            - loginResponse: Derived from the networking module. Encapsulates logged in user.
+     */
     private func goToUserAccounts(loginResponse: LoginResponse.User) {
-        print("going to user accounts")
         let userAccountsViewModel = UserAccountsViewModel(loginResponse: loginResponse)
         userAccountsViewModel.appCoordinator = self
         let userAccountsViewController = UserAccountsViewController(viewModel: userAccountsViewModel)
         let userAccountsNavigationController = UINavigationController(rootViewController: userAccountsViewController)
         navigationController = userAccountsNavigationController
-        if window != nil {
-            print("window is valid")
-        } else {
-            print("window is invalid")
-        }
         window?.rootViewController = userAccountsNavigationController
     }
     
+    /**
+     Method that takes us to Login view controller
+     */
     private func goToLogin() {
         let loginViewModel = LoginViewModel(appCoordinator: self)
         let loginViewController = LoginViewController(viewModel: loginViewModel)
-        //loginViewController.viewModel.appCoordinator = self
         window?.rootViewController = loginViewController
     }
     
+    /**
+     Method that takes us to UserAccountDetails view controller.
+        - Parameters:
+            - productResponse: Derived from the networking module. Contains details about a single user product.
+     */
     private func goToUserAccountDetail(productResponse: ProductResponse) {
         let userAccountDetailViewModel = UserAccountsDetailViewModel(productResponse: productResponse)
         let userAccountDetailViewController = UserAccountDetailViewController(viewModel: userAccountDetailViewModel)
         navigationController?.pushViewController(userAccountDetailViewController, animated: true)
     }
     
+    /**
+     Method that takes us to a designated destination view controller.
+        - Parameters:
+            - destination: An enum type Destination representing one of several view controllers that we can navigate to
+            - data: Any accompanying data needed when navigating to a view controller
+     */
     func proceed(to destination: Destination, data: Any?) {
         switch destination {
         case .login:
             goToLogin()
         case .userAccounts:
-            print("should show user accounts")
             if let loginResponseUser = data as? LoginResponse.User {
                 goToUserAccounts(loginResponse: loginResponseUser)
             }
